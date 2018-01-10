0e361f63f633febaf8c12b20ab77c2b2ebd9db81
Aashni Shah <contact@aashni.me>
2018-01-10 00:16:16 -0500

    adding parentCategoryId check in main controller

--- ajs/controllers/mainCtrl.js
+++ ajs/controllers/mainCtrl.js
@@ -14,20 +14,19 @@ angularApp.controller('MainCtrl', [
     syncObject.$loaded().then(function() {
       for(brand in syncObject.brands) {
           var temp = syncObject.brands[brand];
-          console.log(temp.name);
           $scope.brands.push(temp);
       }
 
       for(product in syncObject.products) {
           var temp = syncObject.products[product];
-          console.log(temp.name);
           $scope.products.push(temp);
       }
 
       for(category in syncObject.categories) {
           var temp = syncObject.categories[category];
-          console.log(temp.name);
-          $scope.categories.push(temp);
+          if(temp.parentCategoryId == 0) {
+            $scope.categories.push(temp);
+          }
       }
 
       $scope.loaded = true;
