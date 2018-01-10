73c5a3553c28ce79eb5bd10726ca59721a54921e
Aashni Shah <contact@aashni.me>
2018-01-09 23:43:20 -0500

    productCtrl cleanup

--- ajs/controllers/productCtrl.js
+++ ajs/controllers/productCtrl.js
@@ -18,6 +18,7 @@ angularApp.controller('ProductCtrl', [
               return
           }
           
+          // TODO: CHECK FOR BRAND ID IN PRODUCTD DETAILS BEFORE ASSIGNING BRAND DETAILS
           $scope.brandDetails = syncObject.brands[$scope.productDetails.brandId];
 
           $scope.loaded = true;
@@ -30,19 +31,7 @@ angularApp.controller('ProductCtrl', [
          *          - link to brand page
          *          - include URL for product purchasing page in the database
          *       3. display those product details
-         */
-
-
-        $scope.productDetails1 = {
-            "brandId" : 1,
-            "categoryId" : 1,
-            "description" : "Delicious milk chocolate bar mixed with hazelnut for an added crunch",
-            "id" : 1,
-            "image" : "https://www.cadbury.com.au/admin/file/content2/c3/_0009_58011352_v1_DES_200G_HAZELNUT_BLOCK_2D.jpg",
-            "name" : "Cadbury Hazelnut",
-            "brandName": "Cadbury",
-            "purchaseUrl": "https://www.cadbury.co.uk/"
-          } 
+         */ 
   
     }
   ]);
