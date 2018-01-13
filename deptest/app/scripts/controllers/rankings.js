82f76babf13e886a9636ce8e6778193211b439a1
Aashni Shah <contact@aashni.me>
2018-01-13 03:53:09 -0500

    adding ranking page

---
+++ deptest/app/scripts/controllers/rankings.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name endslaverynowApp.controller:RankingsCtrl
+ * @description
+ * # RankingsCtrl
+ * Controller of the endslaverynowApp
+ */
+angular.module('endslaverynowApp')
+  .controller('RankingsCtrl', function () {
+  
+  });
---
+++ deptest/test/spec/controllers/rankings.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('Controller: RankingsCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('endslaverynowApp'));
+
+  var RankingsCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    RankingsCtrl = $controller('RankingsCtrl', {
+      $scope: scope
+      // place here mocked dependencies
+    });
+  }));
+
+  it('should attach a list of awesomeThings to the scope', function () {
+    expect(RankingsCtrl.awesomeThings.length).toBe(3);
+  });
+});
