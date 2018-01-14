479465b016d1c952f0ed9393200673e794763bac
Aashni Shah <contact@aashni.me>
2018-01-14 09:54:52 -0500

    fixed grunt build for fonts

--- Gruntfile.js
+++ Gruntfile.js
@@ -396,6 +396,11 @@ module.exports = function (grunt) {
           cwd: 'bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: 'app',
+          src: 'fonts/*',
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
