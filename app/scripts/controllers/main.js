8448e5ad57577c2b898450c709c5c6a10af76f2b
Torie Joy-Warren <torie@carezone.com>
2018-11-13 19:32:08 -0500

    Remove debugger

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,31 +17,30 @@ angular.module('endslaverynowApp')
 			$scope.brands = []
 			$scope.products = []
 			$scope.categories = []
-  
+
 			/* firebase */
 			var firebase = new Firebase(CONFIG.FIREBASEURL)
 			var syncObject = $firebaseObject(firebase)
-      
+
 			syncObject.$loaded().then(function() {
 				for(var brand in syncObject.brands) {
 					var tempBrand = syncObject.brands[brand]
 					$scope.brands.push(tempBrand)
 				}
-  
+
 				for(var product in syncObject.products) {
 					var tempProd = syncObject.products[product]
 					$scope.products.push(tempProd)
 				}
-  
+
 				for(var category in syncObject.categories) {
 					var tempCat = syncObject.categories[category]
-					debugger
 					if (!(tempCat.parentCategoryId > 0)) {
 						$scope.categories.push(tempCat)
 					}
 				}
-  
+
 				$scope.loaded = true
 			})
-      
+
 		}])
