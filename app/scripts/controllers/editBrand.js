89cbf9148b547fbec5304924bb353a77a23d9e79
Torie Joy-Warren <torie@carezone.com>
2018-11-23 17:39:01 -0500

    Allow images to be edited on brands

--- app/scripts/controllers/editBrand.js
+++ app/scripts/controllers/editBrand.js
@@ -18,49 +18,29 @@ angular.module('endslaverynowApp').controller('EditBrandCtrl', [
 		var syncObject = $firebaseObject(firebase)
 		$scope.products = []
 
-	
-
-		
-
 		$scope.processForm = function() {
-			console.log($scope.NameValue)
-
 			if ($scope.NameValue) {
 				syncObject.brands[$scope.brandId].name = $scope.NameValue
 			}
 			if ($scope.DescriptionValue) {
 				syncObject.brands[$scope.brandId].description = $scope.DescriptionValue
 			}
-			
-			if ($scope.CategoryValue) {
-				// syncObject.ranking = $scope.CategoryValue
-			}
 			if ($scope.selectedRankName){
 				syncObject.brands[$scope.brandId].ranking = $scope.selectedRankName
 			}
-			if($scope.selectedCategoryId)
-			{
+			if ($scope.selectedCategoryId) {
 				syncObject.brands[$scope.brandId].categories = $scope.selectedCategoryId
 			}
-
-
-			syncObject.$save().then(
-				function() {
-                    
-					console.log('Done') // true
-					window.alert('Edit has been completed!')
-					window.location.reload()
-                
-				},
-				function(error) {
-					window.alert('Error:', error)
-				}
-			)
+			if ($scope.Image) {
+				syncObject.brands[$scope.brandId].image = $scope.Image
+				uploadImages(syncObject.brands[$scope.brandId], CONFIG.APPCONFIG, 'brand', syncObject)
+			} else {
+				saveSyncObject(syncObject, 'Edit has been completed!')
+			}
 		}
 
-		
+
 		syncObject.$loaded().then(function() {
-			console.log(syncObject)
 			$scope.brands = syncObject.brands
 			$scope.categories = syncObject.categories
 			$scope.products = syncObject.products
@@ -72,7 +52,7 @@ angular.module('endslaverynowApp').controller('EditBrandCtrl', [
 			$scope.rankingOptions = { good: 'Good', better: 'Better', best: 'Best' }
 			$scope.CategoryId = syncObject.brands[$scope.brandId].categories
 			$scope.cat = syncObject.categories[$scope.CategoryId]
-			
+
 			$scope.setRanking = function (rank) {
 				$scope.selectedRankName = rank
 			}
@@ -80,7 +60,7 @@ angular.module('endslaverynowApp').controller('EditBrandCtrl', [
 				$scope.selectedCategoryId = category.id
 				$scope.selectedCategoryName = category.name
 			}
-			
+
 
 			syncObject.$save().then(
 				function() {
